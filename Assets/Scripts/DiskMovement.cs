1d97a3d44df63ef1d1b0c5d7b84a2193df0ef24e
YusufFatihSisman <yusuf.fatih.sisman@gmail.com>
2023-04-11 18:16:41 +0300

    Disk Rotation Added

--- Assets/Scripts/DiskMovement.cs
+++ Assets/Scripts/DiskMovement.cs
@@ -6,6 +6,7 @@ public class DiskMovement : MonoBehaviour
 {
 
     public float scrollSpeed = 1f;
+    public float rotationSpeed = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,12 @@ public class DiskMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(transform.position.x - (Time.deltaTime * scrollSpeed), transform.position.y);
+        transform.RotateAround(transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
     }
 
     void FixedUpdate()
     {
-        transform.position = new Vector2(transform.position.x - (Time.deltaTime * scrollSpeed), transform.position.y);
+        
     }
 }
